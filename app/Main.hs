01d2facae799fda81559cf8244e3d53790c14cd4
SilasMarvin <19626586+SilasMarvin@users.noreply.github.com>
2023-04-27 19:56:37 -0700

    Working

--- app/Main.hs
+++ app/Main.hs
@@ -2,84 +2,112 @@ module Main where
 
 import Control.Monad.State
 import qualified Data.Map as Map
-import Debug.Trace (trace)
 
-data (Num a, Eq a) => Tensor0D a = Tensor0D
+data (Fractional a, Eq a) => Tensor0D a = Tensor0D
   { tid :: Int,
     value :: a
   }
   deriving (Show, Eq)
 
 data Operator = MP | DV | AD | NA
-  deriving (Show, Eq)
+  deriving (Eq)
 
-data (Num a, Eq a) => Operation a = Operation Operator (Tensor0D a) (Tensor0D a) (Tensor0D a) 
-  deriving (Show, Eq)
+data (Fractional a, Eq a) => Operation a = Operation Operator (Tensor0D a) (Tensor0D a) (Tensor0D a)
+  deriving (Eq)
 
-data (Num a, Eq a) => Tape a = Tape 
+data (Fractional a, Eq a) => Tape a = Tape
   {
     operations:: [Operation a],
     nextTensorId:: Int
   }
-  deriving (Show)
+newTape = Tape [] 0
 
-data (Num a, Eq a) => TensorTree a = Empty | Cons (Tensor0D a) Operator (TensorTree a) (TensorTree a) deriving (Eq)
+data (Fractional a, Eq a) => TensorTree a = Empty | Cons (Tensor0D a) Operator (TensorTree a) (TensorTree a) deriving (Eq)
 
-mul :: (Num a, Eq a) => Tensor0D a -> Tensor0D a -> State (Tape a) (Tensor0D a)
-mul t1@Tensor0D { tid = id1, value = value1 } t2@Tensor0D { tid = id2, value = value2 } = do
+createTensor :: (Fractional a, Eq a) => a -> State (Tape a) (Tensor0D a)
+createTensor value = do
+  tape <- get
+  let tensorId = nextTensorId tape
+  put $ tape {nextTensorId = tensorId + 1}
+  return $ Tensor0D tensorId value 
+
+applyGrads :: (Fractional a) => Operator -> a -> a -> a -> (a, a)
+applyGrads op parentGrads leftValue rightValue
+  | op == MP = (parentGrads * rightValue, parentGrads * leftValue)
+  | op == DV = (parentGrads * (1 / rightValue), parentGrads * (-1) * (leftValue / (rightValue * rightValue)))
+  | op == AD = (parentGrads, parentGrads)
+
+tMul :: (Fractional a, Eq a) => Tensor0D a -> Tensor0D a -> State (Tape a) (Tensor0D a)
+tMul t1@Tensor0D { tid = id1, value = value1 } t2@Tensor0D { tid = id2, value = value2 } = do
   tape <- get
   let tensorId = nextTensorId tape
   let ops = operations tape
   let newTensor = Tensor0D tensorId (value1 * value2)
   put $ tape {nextTensorId = tensorId + 1, operations = Operation MP newTensor t1 t2 : ops}
-  return newTensor 
+  return newTensor
 
-appendTree :: (Num a, Eq a) => Operation a -> TensorTree a -> TensorTree a
+tDiv :: (Fractional a, Eq a) => Tensor0D a -> Tensor0D a -> State (Tape a) (Tensor0D a)
+tDiv t1@Tensor0D { tid = id1, value = value1 } t2@Tensor0D { tid = id2, value = value2 } = do
+  tape <- get
+  let tensorId = nextTensorId tape
+  let ops = operations tape
+  let newTensor = Tensor0D tensorId (value1 / value2)
+  put $ tape {nextTensorId = tensorId + 1, operations = Operation DV newTensor t1 t2 : ops}
+  return newTensor
+
+tAdd :: (Fractional a, Eq a) => Tensor0D a -> Tensor0D a -> State (Tape a) (Tensor0D a)
+tAdd t1@Tensor0D { tid = id1, value = value1 } t2@Tensor0D { tid = id2, value = value2 } = do
+  tape <- get
+  let tensorId = nextTensorId tape
+  let ops = operations tape
+  let newTensor = Tensor0D tensorId (value1 + value2)
+  put $ tape {nextTensorId = tensorId + 1, operations = Operation AD newTensor t1 t2 : ops}
+  return newTensor
+
+appendTree :: (Fractional a, Eq a) => Operation a -> TensorTree a -> TensorTree a
 appendTree (Operation op t1 t2 t3) Empty = Cons t1 op (Cons t2 NA Empty Empty) (Cons t3 NA Empty Empty)
 appendTree fullOp@(Operation op t1@Tensor0D { tid = opId } t2 t3) tree@(Cons treeTop@Tensor0D { tid = id } treeOp leftTree@(Cons Tensor0D { tid = leftId, value = leftValue } _ _ _) rightTree@(Cons Tensor0D { tid = rightId, value = rightValue } _ _ _))
-  | opId == leftId = Cons treeTop treeOp (Cons t1 op (Cons t2 NA Empty Empty) (Cons t3 NA Empty Empty)) rightTree 
+  | opId == leftId = Cons treeTop treeOp (Cons t1 op (Cons t2 NA Empty Empty) (Cons t3 NA Empty Empty)) rightTree
   | opId == rightId = Cons treeTop treeOp leftTree (Cons t1 op (Cons t2 NA Empty Empty) (Cons t3 NA Empty Empty))
-  | otherwise = 
+  | otherwise =
     let newLeftTree = appendTree fullOp leftTree
         newRightTree = appendTree fullOp rightTree
     in if newLeftTree /= leftTree
-          then Cons treeTop treeOp newLeftTree rightTree    
+          then Cons treeTop treeOp newLeftTree rightTree
           else Cons treeTop treeOp leftTree newRightTree
-appendTree _ tree@(Cons _ _ Empty Empty) = tree 
+appendTree _ tree@(Cons _ _ Empty Empty) = tree
 
-buildTree :: (Num a, Eq a) => [Operation a] -> TensorTree a -> TensorTree a
-buildTree (x:y) tree = buildTree y $ appendTree x tree 
+buildTree :: (Fractional a, Eq a) => [Operation a] -> TensorTree a -> TensorTree a
+buildTree (x:y) tree = buildTree y $ appendTree x tree
 buildTree _ tree = tree
 
-backTree :: (Num a, Eq a, Show a) => TensorTree a -> Map.Map Int a -> Map.Map Int a
-backTree (Cons Tensor0D { tid = id } op leftTree@(Cons Tensor0D { tid = leftId, value = leftValue } _ _ _) rightTree@(Cons Tensor0D { tid = rightId, value = rightValue } _ _ _)) map = 
-  let pGrads = trace ("\nbackTree " ++ show id ++ " " ++ show leftId ++ " " ++ show rightId ++ " "++ show (Map.findWithDefault 1 id map)) (Map.findWithDefault 1 id map)
-      leftGrads = trace ("leftGrads " ++ show (pGrads * rightValue)) pGrads * rightValue
-      rightGrads = trace ("rightGrads " ++ show (pGrads * leftValue)) pGrads * leftValue
+backTree :: (Fractional a, Eq a) => TensorTree a -> Map.Map Int a -> Map.Map Int a
+backTree (Cons Tensor0D { tid = id } op leftTree@(Cons Tensor0D { tid = leftId, value = leftValue } _ _ _) rightTree@(Cons Tensor0D { tid = rightId, value = rightValue } _ _ _)) map =
+  let pGrads = Map.findWithDefault 1 id map
+      (leftGrads, rightGrads) = applyGrads op pGrads leftValue rightValue 
       leftMap = Map.delete id $ Map.insert leftId leftGrads map
       rightMap = Map.insert rightId rightGrads map
-  in Map.unionWith (+) (backTree leftTree leftMap) (backTree rightTree rightMap) 
+  in Map.unionWith (+) (backTree leftTree leftMap) (backTree rightTree rightMap)
 backTree (Cons Tensor0D { tid = id } op Empty Empty) map = map
 
-backward :: (Num a, Eq a, Show a) => State (Tape a) (Map.Map Int a)
+backward :: (Fractional a, Eq a) => State (Tape a) (Map.Map Int a)
 backward = do
   tape <- get
   let ops = operations tape
   let tree = buildTree ops Empty
   return $ backTree tree Map.empty
 
-doComputations :: (Num a, Eq a, Show a) => State (Tape a) (Tensor0D a, Map.Map Int a)
+doComputations :: (Fractional a, Eq a) => State (Tape a) (Tensor0D a, Map.Map Int a)
 doComputations = do
-  let t0 = Tensor0D 0 1
-  let t1 = Tensor0D 1 2
-  t2 <- mul t0 t1
-  t3 <- mul t1 t2
-  t4 <- mul t2 t3
-  t5 <- mul t4 t3
+  t0 <- createTensor 1 
+  t1 <- createTensor 2
+  t2 <- tDiv t1 t0
+  t3 <- tAdd t2 t1
   grads <- backward
-  return (t5, grads)
+  return (t3, grads)
 
 main :: IO ()
 main = do
-  let out = evalState doComputations (Tape [] 2)
-  print out
+  let (tensor, grads) = evalState doComputations newTape 
+  print tensor 
+  print grads
